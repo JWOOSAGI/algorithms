e62497d80c518bd2b50d965fce0f289f2780485c
JWOOSAGI <ddgfg3234@naver.com>
2024-02-28 21:23:04 +0900

    20240228 change

--- changeMission/Change.java
+++ changeMission/Change.java
@@ -15,6 +15,5 @@ public class Change {
             }
         }
         System.out.println(change);
-        
     }
 }
--- changeMission/ChangeTest.java
+++ changeMission/ChangeTest.java
@@ -7,9 +7,9 @@ public class ChangeTest {
         Scanner sc = new Scanner(System.in);
         int i = 1000 - sc.nextInt();
         int change = 0;
-        int[] arr = {500,100,50,10,5,1};
-        for(int j=0;j< arr.length;j++){
-            if (i % arr[j] >= 0){
+        int[] arr = {500, 100, 50, 10, 5, 1};
+        for (int j = 0; j < arr.length; j++) {
+            if (i % arr[j] >= 0) {
                 change += i / arr[j];
                 i = i % arr[j];
             }
