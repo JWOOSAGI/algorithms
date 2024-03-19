38329cc082732aeac401c3d82144d6d72b326c06
JWOOSAGI <ddgfg3234@naver.com>
2024-03-19 19:54:52 +0900

    20240319 honey

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        if (a > b){
-            System.out.println(">");
-        } else if (a < b) {
-            System.out.println("<");
-        } else {
-            System.out.println("==");
+        int N = sc.nextInt();
+
+        int[] num = new int[N + 1];	// F(0) 부터 시작하므로 N + 1 크기 생성
+
+        for(int i = 0; i < num.length; i++) {
+            // F(0) 과 F(1) 은 각각 0 과 1 로 초기화
+            if(i == 0) num[0] = 0;
+            else if(i == 1) num[1] = 1;
+            else num[i] = num[i - 1] + num[i - 2];
         }
+
+        System.out.println(num[N]);
+
     }
 }
