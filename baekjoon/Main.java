759b781288730c463ffa355b33f7154366b5ab9a
JWOOSAGI <101155291+JWOOSAGI@users.noreply.github.com>
2024-03-23 12:05:12 +0900

    20240323 baekjoon

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = a+b;
-        int d = a-b;
-        int e = a*b;
-        int f = a/b;
-        int g = a%b;
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-    }
 
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] num = new int[N + 1];	// F(0) 부터 시작하므로 N + 1 크기 생성
+
+        for(int i = 0; i < num.length; i++) {
+            // F(0) 과 F(1) 은 각각 0 과 1 로 초기화
+            if(i == 0) num[0] = 0;
+            else if(i == 1) num[1] = 1;
+            else num[i] = num[i - 1] + num[i - 2];
+        }
+
+        System.out.println(num[N]);
+
+    }
 }
