6b75dc6171a824f5d8a55a4194b57d52e4813fd9
JWOOSAGI <ddgfg3234@naver.com>
2024-03-25 18:35:15 +0900

    20240325 baekjoon

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -6,16 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int c = a+b;
-        int d = a-b;
-        int e = a*b;
-        int f = a/b;
-        int g = a%b;
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
+        System.out.println(a+b);
     }
 
 }
