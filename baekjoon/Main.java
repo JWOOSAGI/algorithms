d45f99e9ae0382292020759465ad6ba9aea31186
JWOOSAGI <ddgfg3234@naver.com>
2024-04-03 20:05:02 +0900

    baekjoon

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,13 +1,16 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n == 0) {  //0 일때 영문명 출력
+            System.out.print("YONSEI");
+        } else {  //N = 1일때 슬로건 출력
+            System.out.print("Leading the Way to the Future");
+        }
     }
 }
-
