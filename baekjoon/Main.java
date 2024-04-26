64e9d40d2c8e3e4377fdf7c4a0950df23e1f40cc
JWOOSAGI <101155291+JWOOSAGI@users.noreply.github.com>
2024-04-26 23:21:52 +0900

    20240426 Baekjoon

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,22 +1,20 @@
 package baekjoon;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
 
-        int[] num = new int[N + 1];	// F(0) 부터 시작하므로 N + 1 크기 생성
-
-        for(int i = 0; i < num.length; i++) {
-            // F(0) 과 F(1) 은 각각 0 과 1 로 초기화
-            if(i == 0) num[0] = 0;
-            else if(i == 1) num[1] = 1;
-            else num[i] = num[i - 1] + num[i - 2];
-        }
-
-        System.out.println(num[N]);
-
+        if(N == M) System.out.println(1);
+        else System.out.println(0);
     }
+
 }
