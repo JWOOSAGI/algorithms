6d3ed541edcf3506530e2eb616640cc8a2248af1
JWOOSAGI <ddgfg3234@naver.com>
2024-05-07 18:41:42 +0900

    baekjoon

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,20 +1,12 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        while (true) {
-            int a = sc.nextInt();
-            if (a == -1) {
-                break;
-            }
-
-            sum += a;
-
-        }
-        System.out.println(sum);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(n - 1946);
     }
 }
