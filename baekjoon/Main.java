13c9a6ff0fe8f8eb8342c9ff7a09c90a7df82329
JWOOSAGI <ddgfg3234@naver.com>
2024-05-08 19:32:29 +0900

    baekjoon

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,12 +1,36 @@
 package baekjoon;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.print(n - 1946);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+
+        int sum = S + K + Y;
+        //최솟갑 구하기
+        int min = Math.min(S, K);
+        min = Math.min(min, Y);
+
+        if(sum >= 100) {
+            System.out.println("OK");
+        }else {
+            if(min == S) {
+                System.out.println("Soongsil");
+            }else if(min == K) {
+                System.out.println("Korea");
+            }else {
+                System.out.println("Hanyang");
+            }
+        }
+
     }
+
 }
