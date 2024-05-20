1fdbb9f18aecc2e76495e3f5e8c65b94789c5bb7
JWOOSAGI <ddgfg3234@naver.com>
2024-05-20 18:49:13 +0900

    20240520 baekjoon

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -10,27 +10,12 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int Y = Integer.parseInt(st.nextToken());
+        Long N = Long.parseLong(st.nextToken());
+        Long M = Long.parseLong(st.nextToken());
 
-        int sum = S + K + Y;
-        //최솟갑 구하기
-        int min = Math.min(S, K);
-        min = Math.min(min, Y);
-
-        if(sum >= 100) {
-            System.out.println("OK");
-        }else {
-            if(min == S) {
-                System.out.println("Soongsil");
-            }else if(min == K) {
-                System.out.println("Korea");
-            }else {
-                System.out.println("Hanyang");
-            }
-        }
-
+        System.out.println(((N * M) / 2));
     }
+
+}
 
 }
